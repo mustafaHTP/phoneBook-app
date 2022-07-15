cb87d711dcc63f06be05d29bd72eb6e226e26f9e
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-15 18:30:48 +0300

    Add project files.

---
+++ PhoneBook.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneBook.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
