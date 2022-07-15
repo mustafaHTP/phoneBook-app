8e9a5c75184909a22ed6bcad97b63ee87156c2a0
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-15 20:06:32 +0300

    Updated/Created date added to entity via SaveChanges()

--- PhoneBook.Repository/AppDbContext.cs
+++ PhoneBook.Repository/AppDbContext.cs
@@ -18,7 +18,48 @@ namespace PhoneBook.Repository
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<PhoneNumber> PhoneNumbers { get; set; }
 
+        //add Updated/Created Date to entity
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var item in ChangeTracker.Entries())
+            {
+                if(item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Added:
+                            entityReference.CreatedDate = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Added:
+                            entityReference.CreatedDate = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
