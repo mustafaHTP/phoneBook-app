3133d41df8acc0819a5780d644171de9de996916
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-15 18:42:51 +0300

    Models addded

---
+++ PhoneBook.Core/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Core.Models
+{
+    public class Contact : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Profession { get; set; }
+        public string Address { get; set; }
+        public string WebAddress { get; set; }
+        public string NickName { get; set; }
+        public ICollection<PhoneNumber> PhoneNumbers { get; set; }
+
+    }
+}
+
---
+++ PhoneBook.Core/Models/PhoneNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Core.Models
+{
+    public class PhoneNumber
+    {
+        public int ContactId { get; set; }
+        public Contact Contact { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
