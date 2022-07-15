aedd29e95c58813d5a2fcdc0e43ab34296f9e17f
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-15 18:57:15 +0300

    IGenericService added

---
+++ PhoneBook.Core/Services/IGenericService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Core.Services
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        // productRepository.where(x => x.id > 5). after tolist make a query
+        // productRepository.where(x => x.id > 5).OrderBy().ToListAsync();
+        // IQueryable objects, is not going to be queried, in memory
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
