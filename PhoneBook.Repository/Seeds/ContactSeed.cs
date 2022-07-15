e6afa966d01a40fa1feb645a7f2425356c3e0856
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-15 19:30:32 +0300

    Seeds Data added

---
+++ PhoneBook.Repository/Seeds/ContactSeed.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoneBook.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Repository.Seeds
+{
+    internal class ContactSeed : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.HasData(
+                new Contact
+                {
+                    Id = 1,
+                    FirstName = "Mustafa",
+                    LastName = "Hatipoğlu",
+                    Email = "mustafa@outlook.com",
+                    Profession = "Student",
+                    Address = "Üsküdar/İstanbul",
+                    WebAddress = "www.api.com",
+                    NickName = "mustafa",
+                    CreatedDate = DateTime.Now
+                },
+                new Contact
+                {
+                    Id = 2,
+                    FirstName = "Ahmet",
+                    LastName = "Yılmaz",
+                    Email = "ahmet@gmail.com",
+                    Profession = "Student",
+                    Address = "Ümraniye/İstanbul",
+                    WebAddress = "www.google.com",
+                    NickName = "ahmet",
+                    CreatedDate = DateTime.Now
+                }
+                );
+        }
+    }
+}
---
+++ PhoneBook.Repository/Seeds/PhoneNumberSeed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoneBook.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Repository.Seeds
+{
+    internal class PhoneNumberSeed : IEntityTypeConfiguration<PhoneNumber>
+    {
+        public void Configure(EntityTypeBuilder<PhoneNumber> builder)
+        {
+            builder.HasData(
+                new PhoneNumber
+                {
+                    Id = 1,
+                    ContactId = 1,
+                    PhoneNo = "05426652233",
+                    CreatedDate = DateTime.Now
+                },
+                new PhoneNumber
+                {
+                    Id = 2,
+                    ContactId = 1,
+                    PhoneNo = "05384332199",
+                    CreatedDate = DateTime.Now
+                },
+                new PhoneNumber
+                {
+                    Id = 3,
+                    ContactId = 2,
+                    PhoneNo = "05406004030",
+                    CreatedDate = DateTime.Now
+                },
+                new PhoneNumber
+                {
+                    Id = 4,
+                    ContactId = 2,
+                    PhoneNo = "05332221036",
+                    CreatedDate = DateTime.Now
+                }
+            );
+        }
+    }
+}
