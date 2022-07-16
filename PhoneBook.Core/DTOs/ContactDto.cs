bac1caa507a83cf455a22b94080a8d708d424f44
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2022-07-16 13:15:20 +0300

    Dtos added

---
+++ PhoneBook.Core/DTOs/ContactDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Core.Models
+{
+    public class ContactDto : BaseDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Profession { get; set; }
+        public string Address { get; set; }
+        public string WebAddress { get; set; }
+        public string NickName { get; set; }
+
+    }
+}
+
---
+++ PhoneBook.Core/DTOs/PhoneNumberDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Core.Models
+{
+    public class PhoneNumberDto: BaseDto
+    {
+        public int ContactId { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
